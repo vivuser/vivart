e88f0306dde9e3953103e1d0513bc1ec6a742459
vivuser <vivekchamyal41@gmail.com>
2024-06-02 16:05:04 +0530

    editor improvement

--- app/components/Logout.jsx
+++ app/components/Logout.jsx
@@ -4,14 +4,23 @@ import { useDispatch} from "react-redux";
 import { logout } from "@/app/redux/slices/authSlice";
 import { signOut } from "next-auth/react";
 import { redirect, useRouter} from "next/navigation";
+import AutohideSnackbar from "./Snackbar";
+import { openSnackbar } from "../redux/slices/commonSlice";
 
 
 const Logout = () => {
     const router = useRouter();
+    const dispatch = useDispatch();
 
     const handleLogout = async () => {
         try {
             await signOut({ callbackUrl:'/'})
+            dispatch (
+                openSnackbar({
+                    content : 'Logged-out successfully',
+                    color: 'success'
+                })
+            )
             }
             catch(error){ 
                 console.error('Logout failed', error);
@@ -20,9 +29,10 @@ const Logout = () => {
 
 
 
-    return (
+    return (<>
+        <AutohideSnackbar />
         <button onClick={handleLogout} className="underline underline-offset-2 m-2 text-slate-700">Logout</button>
-    )
+        </>)
 }
 
 export default Logout;
