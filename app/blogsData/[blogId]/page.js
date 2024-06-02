dd83fcf8ad5b0eee020f1668ee773ab764695373
vivuser <vivekchamyal41@gmail.com>
2024-06-02 17:43:05 +0530

    incrementing logic

--- app/blogsData/[blogId]/page.js
+++ app/blogsData/[blogId]/page.js
@@ -2,6 +2,7 @@ import getSingleBlog from "@/app/redux/apis/singleBlogApi";
 import { Suspense } from "react";
 import SinglePost from "./components/SinglePost";
 import getAllBlogs from "@/app/redux/apis/allBlogsApi";
+import axios from "axios";
 
 
 export const metadata =  {
@@ -15,13 +16,16 @@ export default  async function SingleBlogPage({params}) {
     const incrementViews = async () => {
         try {
             console.log('incrementing view')
-            await axios.post(`http://localhost:3001/blogs/${params.id}/increment-views`)
+            await axios.post(`http://localhost:3001/blogs/${params.blogId}/increment-views`)
         } catch (error) {
             console.error('Error incrementing views', error)
-            console.log(params.id, 'paramsId')
+            console.log(params.blogId, 'paramsId')
 
         }
     }
+
+    incrementViews();
+    
 
     const singleBlogContent = (
         <div className="m-10 flex items-center">
