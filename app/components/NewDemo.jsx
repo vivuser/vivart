2e283f9bd33f55f32dbd75dfd297096f4f260510
vivuser <vivekchamyal41@gmail.com>
2024-08-18 22:07:50 +0530

    created new component Demo

---
+++ app/components/NewDemo.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+
+const NewComponent = () => {
+
+    const [count, setCount] = useState('')
+
+    
+
+    return (
+        <div>
+            {count}
+        </div>
+    )
+
+}
+
+export default NewComponent
