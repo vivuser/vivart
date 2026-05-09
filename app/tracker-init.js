23d0713b2de74e232d7312d2903289d3caf9cda2
vivuser <vivekchamyal41@gmail.com>
2026-05-10 00:38:50 +0530

    shjd

--- app/tracker-init.js
+++ app/tracker-init.js
@@ -4,7 +4,7 @@ import { initTracker } from "@/blocker-tracker/tracker.mjs";
 if (typeof window !== "undefined") {
   initTracker({
     endpoint: "http://localhost:3001/sdk/blockers",
-  apiKey: "proj_5874c607b8651ad5",
-  environment: "production"
+    apiKey: "proj_430a2d7af64c39b6",
+    environment: "production"
   });
 }
